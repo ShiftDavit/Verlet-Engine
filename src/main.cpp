a9d19804f9a16e119511785cc4b4df1fd7b37167
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-05-10 00:37:06 -0700

    Added bottom bounds checking

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,43 @@
 #include <vector>
 #include <string>
 #include "raylib.h"
+#include "math/Vec2.h"
 
 using std::vector;
 
-constexpr float G = 9.8f;
-constexpr int WIN_WIDTH = 1200;
-constexpr int WIN_HEIGHT = 800;
+constexpr float G { 9.8f };
+constexpr int WIN_WIDTH { 1200 };
+constexpr int WIN_HEIGHT { 800 };
+constexpr float PHYSICS_STEP { 1.f/60 };
 
 struct Particle {
-    float x;
-    float y;
-
-    float vx;
-    float vy;
+    Vec2 pos;
+    Vec2 prevPos;
+    Vec2 accel;
 
     float radius;
 };
 
-void updateParticles(vector<Particle>& parts, float dt=1.f/60){
-    for (auto& p : parts){
-        p.vy += G * dt;
+void solve(vector<Particle>& parts, float dt=PHYSICS_STEP){
+    Vec2 tmp{};
+    Vec2 nextP{};
 
-        p.x += p.vx * dt;
-        p.y += p.vy * dt;
+    for (auto& p : parts){
+        tmp = p.pos;
+        nextP = p.pos * 2 - p.prevPos + p.accel * dt * dt;
+
+        p.pos = nextP;
+        p.prevPos = tmp;
+
+        if (p.pos.y > WIN_HEIGHT - p.radius){
+            p.prevPos = p.pos + p.pos - p.prevPos;
+        }
     }
 }
 
 void renderParticles(vector<Particle>& parts){
     for (auto& p : parts){
-        DrawCircle(p.x, p.y, p.radius, WHITE);
+        DrawCircle(p.pos.x, p.pos.y, p.radius, WHITE);
     }
 }
 
@@ -41,46 +49,34 @@ void showFps(float dt){
 
 int main(){
     InitWindow(WIN_WIDTH, WIN_HEIGHT, "raylib test");
-    SetTargetFPS(240);
+    SetTargetFPS(60);
 
     vector<Particle> parts {
-        Particle {
-            WIN_WIDTH/3,
-            WIN_HEIGHT/2,
-
-            10,
-            -10,
-
-            10
-        },
-
-        Particle {
-            2 * WIN_WIDTH/3,
-            WIN_HEIGHT/2,
-
-            -5,
-            -15,
-
-            10
-        },
-
-        Particle {
-            WIN_WIDTH/2,
-            WIN_HEIGHT/2,
-
-            0,
-            0,
-
-            10
-        },
+        
     };
     
-    float dt;
+    float dt{};
     while (!WindowShouldClose()) {
 
         dt = GetFrameTime();
+
+        if (IsMouseButtonDown(0)){
+            parts.push_back(Particle {
+                Vec2{(float)GetMouseX(),
+                (float)GetMouseY(),},
+                Vec2{(float)GetMouseX(),
+                (float)GetMouseY(),},
+
+                Vec2{0,G},
+
+                (float)(rand() % 10 + 10)
+            });
+        }
+
         // Solve
-        updateParticles(parts, dt*5);
+        for (int i = 0; i < 6; ++i){
+            solve(parts, dt);
+        }
         // Render
         BeginDrawing();
         ClearBackground(BLACK);
