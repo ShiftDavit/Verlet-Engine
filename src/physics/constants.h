32776c6c632716a3cea243e5a1e956b23a55cfb6
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-05-11 15:50:51 -0700

    Added global friction coefficient

--- src/physics/constants.h
+++ src/physics/constants.h
@@ -2,5 +2,6 @@
 
 namespace physics{
     inline constexpr float G { 9.8f };
-    inline constexpr float PHYSICS_STEP { 1.f/60 };
+    inline constexpr float PHYSICS_STEP { 1.f/120 };
+    inline constexpr float FRICTION { .6f };
 }
