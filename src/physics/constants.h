efe4ff8619f6051ff7a555f615ae147af092dc47
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-05-11 16:01:16 -0700

    Increased gravity to signigicant amount

--- src/physics/constants.h
+++ src/physics/constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace physics{
-    inline constexpr float G { 9.8f };
-    inline constexpr float PHYSICS_STEP { 1.f/120 };
+    inline constexpr float G { 1000.0f };
+    inline constexpr float PHYSICS_STEP { 1.f/60 };
     inline constexpr float FRICTION { .6f };
 }
