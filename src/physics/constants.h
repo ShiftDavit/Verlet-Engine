b02d120ff91fb3161c0028b55710693aa855205f
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-05-13 23:41:56 -0700

    Formatting

--- src/physics/constants.h
+++ src/physics/constants.h
@@ -1,7 +1,8 @@
 #pragma once
 
-namespace physics{
-    inline constexpr float G { 10000.0f };
-    inline constexpr float PHYSICS_STEP { 1.f/60 };
-    inline constexpr float FRICTION { .6f };
+namespace physics
+{
+    inline constexpr float G{10000.0f};
+    inline constexpr float PHYSICS_STEP{1.f / 60};
+    inline constexpr float FRICTION{.6f};
 }
