7948e5ef50ee56a7fcf85920a94d30cceff67d54
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-05-13 23:40:22 -0700

    Particle & constraint addition methods

--- src/state/World.h
+++ src/state/World.h
@@ -6,7 +6,11 @@
 #include <vector>
 #include <memory>
 
-struct World{
-    std::vector<Particle>& particles;
-    std::vector<Constraint*>& constraints;
+struct World
+{
+    std::vector<Particle> particles;
+    std::vector<Constraint *> constraints;
+
+    void add(Particle &p) { particles.push_back(p); };
+    void add(Constraint &c) { constraints.push_back(&c); };
 };
