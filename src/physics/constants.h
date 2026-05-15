194587bf35a6a28c5126585cca760d3eaa40581c
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-05-14 17:36:40 -0700

    Remove old src

--- src/physics/constants.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace physics
-{
-    inline constexpr float G{10000.0f};
-    inline constexpr float PHYSICS_STEP{1.f / 60};
-    inline constexpr float FRICTION{.6f};
-}
--- src/render/Renderer.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "../state/Particle.h"
-#include "../state/World.h"
-
-void drawParticles(World &);
-void drawConstraints(World &world);
