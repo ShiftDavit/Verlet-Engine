9e36fe615e641b86ca63856073ae3c32d77e1b4f
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-08-12 15:07:03 -0700

    Average dt readings for more readability

--- engine/debug/DebugPanel.cpp
+++ engine/debug/DebugPanel.cpp
@@ -2,20 +2,27 @@
 #include "../state/SystemState.h"
 #include "rlImGui.h"
 #include "imgui.h"
-#include <string>
 
-void DrawEngineStats(verlet::World &world, float drawDt, float physicsStepMs)
+void DrawEngineStats(verlet::World &world, const EngineDebugStats &stats)
 {
-    ImGui::Begin("Engine Stats", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+    static float lastDt = 0;
+    float avgDt = (stats.frameDt + lastDt) / 2;
+
+    ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
+    ImGui::Begin("Engine Stats", nullptr, ImGuiWindowFlags_Modal);
     ImGui::SeparatorText("Draw");
-    ImGui::Text("Frame Time: %.3f", drawDt);
-    ImGui::Text("FPS: %.d", (int)(1 / drawDt));
+    ImGui::Text("Frame Time: %.2f ms", avgDt * 1000.f);
+    ImGui::Text("FPS: %d", (int)(1 / avgDt));
+
     ImGui::SeparatorText("Physics");
-    ImGui::BeginGroup();
-    ImGui::EndGroup();
-    ImGui::Text("Step MS: %.3f", physicsStepMs);
+    ImGui::Text("Fixed Step: %.2f ms", stats.fixedPhysicsDt * 1000.f);
+    ImGui::Text("Accumulator: %.2f ms", stats.physicsAccumulatorDt * 1000.f);
+    ImGui::Text("Steps This Frame: %d", stats.physicsSteps);
+
     ImGui::SeparatorText("State");
     ImGui::Text("Particles: %d", world.particles.size());
     ImGui::Text("Constraints: %d", world.constraints.size());
     ImGui::End();
+
+    lastDt = stats.frameDt;
 }
