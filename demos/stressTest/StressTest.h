26bea0b4cdcef34f2cf486d1dac6ce9f8acaf596
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-08-12 15:06:39 -0700

    higher spawn rate

--- demos/stressTest/StressTest.h
+++ demos/stressTest/StressTest.h
@@ -9,7 +9,7 @@ public:
     void OnUpdate(float dt) override;
 
 private:
-    int spawnRate{10};
+    int spawnRate{144};
     bool killed{false};
     float elapsed{};
     float lastSpawnTime{-1};
