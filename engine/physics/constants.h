766165d9186954d14f76c4f5eb1745c58c609a9f
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-08-12 21:18:50 -0700

    Reduced substeps

--- engine/physics/constants.h
+++ engine/physics/constants.h
@@ -5,5 +5,5 @@ namespace verlet
     inline constexpr float G{5000.0f};
     inline constexpr float PHYSICS_STEP{1.f / 60};
     inline constexpr float FRICTION{.5f};
-    inline constexpr int SUBSTEP_COUNT{8};
+    inline constexpr int SUBSTEP_COUNT{4};
 }
