8a3cba86cebd42f358ce4d2b57abfd15c1733f8e
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-08-12 21:15:54 -0700

    Integrated spatial hashing

--- engine/physics/Solver.cpp
+++ engine/physics/Solver.cpp
@@ -1,4 +1,5 @@
 #include "Solver.h"
+#include "CollisionGrid.h"
 #include "constants.h"
 #include "../state/SystemState.h"
 
@@ -32,12 +33,32 @@ void Solver::applyConstraints(World &world)
 
 void Solver::solveCollisions(World &world)
 {
+    float maxRadius = 1.f;
+    for (const auto &p : world.particles)
+    {
+        maxRadius = std::max(maxRadius, p.radius);
+    }
+
+    CollisionGrid grid{maxRadius * 1.5f};
+    for (std::size_t i{0}; i < world.particles.size(); ++i)
+    {
+        grid.add(static_cast<ParticleID>(i), world.particles[i]);
+    }
+
+    std::vector<ParticleID> nearbyParticleIds;
     for (std::size_t i{0}; i < world.particles.size(); ++i)
     {
         Particle &p1 = world.particles[i];
-        for (std::size_t j{i + 1}; j < world.particles.size(); ++j)
+        grid.getNearbyParticleIds(p1.pos, nearbyParticleIds);
+
+        for (ParticleID particleId : nearbyParticleIds)
         {
-            Particle &p2 = world.particles[j];
+            if (particleId <= static_cast<ParticleID>(i))
+            {
+                continue;
+            }
+
+            Particle &p2 = world.particles[particleId];
             const Vec2 collisionAxis = p1.pos - p2.pos;
             const float dist = collisionAxis.magnitude();
             const float combinedRadius = p1.radius + p2.radius;
